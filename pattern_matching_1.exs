0c5680a263e804a47384f51680ca9301d849d240
Armando Couto <coutoarmando@gmail.com>
2020-03-14 16:56:17 -0300

    Ajustes

--- pattern_matching_1.exs
+++ pattern_matching_1.exs
@@ -1,7 +1,14 @@
 a = [1, 2, 3]
+print(a)
 a = 4
+print(a)
 4 = a
+print(4)
 [a, b] = [1, 2, 3] # ** (MatchError) no match of right hand side value: [1, 2, 3]
+print([a, b])
 a = [[1, 2, 3]]
+print(a)
 [a] = [[1, 2, 3]]
+print([a])
 [[a]] = [[1, 2, 3]] # ** (MatchError) no match of right hand side value: [[1, 2, 3]]
+print([[a]])
